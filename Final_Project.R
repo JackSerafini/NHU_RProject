b04a7b795b8124de21ff641c3cc8814ac11bcbb3
MARSICULUS <leonardo.marsich@gmail.com>
2023-06-30 12:50:45 +0200

    Grafici più belli width

--- Final_Project.R
+++ Final_Project.R
@@ -14,54 +14,54 @@ library(cowplot)
 
 
 p1 <- ggplot(data = Data, aes(y = TPY)) +
-    geom_boxplot(fill = "yellow") +
+    geom_boxplot(fill = "yellow",width = 0.5) +
     theme_classic() 
 
 p2 <- ggplot(data = Data, aes(y = NUMBED)) +
-    geom_boxplot(fill = "yellow") +
+    geom_boxplot(fill = "yellow", width = 0.5) +
     theme_classic() 
 
 p3 <- ggplot(data = Data, aes(y = SQRFOOT)) +
-    geom_boxplot(fill = "yellow") +
+    geom_boxplot(fill = "yellow", width = 0.5) +
     theme_classic() 
 
 p4 <- ggplot(data = Data, aes(x = CRYEAR, fill = CRYEAR)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p5 <- ggplot(data = Data, aes(x = MSA, fill = MSA)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p6 <- ggplot(data = Data, aes(x = URBAN, fill = URBAN)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p7 <- ggplot(data = Data, aes(x = PRO, fill = PRO)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p8 <- ggplot(data = Data, aes(x = TAXEXEMPT, fill = TAXEXEMPT)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p9 <- ggplot(data = Data, aes(x = SELFFUNDINS, fill = SELFFUNDINS)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p10 <- ggplot(data = Data, aes(x = MCERT, fill = MCERT)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
 p11 <- ggplot(data = Data, aes(x = ORGSTR, fill = ORGSTR)) +
-    geom_bar() +
+    geom_bar(width = 0.5) +
     theme_classic() +
     theme(legend.position = "none")
 
