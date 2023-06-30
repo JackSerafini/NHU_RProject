20c153b8d69210089155a432bb9b1cf72e4a66ef
JackSerafini <94188919+JackSerafini@users.noreply.github.com>
2023-06-30 11:25:38 +0200

    Added script via upload

---
+++ Final_Project.R
@@ -0,0 +1,8 @@
+Data <- read.csv("~/Documents/Uni/Basi di Dati B/data/WiscNursingHome.csv", header = TRUE)
+# Probabilmente anche CRYEAR: Data$CRYEAR <- factor(Data$CRYEAR)
+Data$URBAN <- factor(Data$URBAN)
+Data$PRO <- factor(Data$PRO)
+Data$TAXEXEMPT <- factor(Data$TAXEXEMPT)
+Data$SELFFUNDINS <- factor(Data$SELFFUNDINS)
+Data$MCERT <- factor(Data$MCERT)
+Data$ORGSTR <- factor(Data$ORGSTR)
